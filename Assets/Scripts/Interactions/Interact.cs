9c520c0903fbda6d77f13b38009131de0fc416f3
Nabski89 <Nabski@Hotmail.com>
2023-01-16 16:27:02 -0500

    InteractMouseover

--- Assets/Scripts/Interactions/Interact.cs
+++ Assets/Scripts/Interactions/Interact.cs
@@ -1,16 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+//using UnityEngine.UI;
 
 public class Interact : MonoBehaviour
 {
     IInteractable InteractObject;
     bool KeyDownEvent = false;
+    public GameObject UITextObj;
+    public TMPro.TextMeshProUGUI UIText;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        UIText = UITextObj.GetComponent<TMPro.TextMeshProUGUI>();
     }
 
     // Keydown outside of update can miss calls
@@ -22,34 +25,38 @@ public class Interact : MonoBehaviour
 
     void FixedUpdate()
     {
-        if (KeyDownEvent == true)
+
+        //clear these out before we cast so we can't pick things back up
+        InteractObject = null;
+        RaycastHit hitInteract;
+
+        // Does the ray intersect any objects excluding the player layer
+        //     Debug.DrawLine(transform.position, transform.position + (transform.forward * 2), Color.white, 10.0f);
+
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hitInteract, 2))
         {
-            KeyDownEvent = false;
-            Debug.Log("Pressed the interact Button");
-            //clear these out before we cast so we can't pick things back up
-            InteractObject = null;
-            RaycastHit hitInteract;
-
-            // Does the ray intersect any objects excluding the player layer
-            //     Debug.DrawLine(transform.position, transform.position + (transform.forward * 2), Color.white, 10.0f);
-
-            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hitInteract, 2))
+            //Debug.Log("Hit Something " + hitInteract.transform);
+            //Debug.DrawRay(transform.position, (transform.forward * 1 * hitInteract.distance), Color.yellow, 10f);
+            InteractObject = hitInteract.transform.GetComponent<IInteractable>();
+            if (InteractObject != null)
             {
-                Debug.Log("Hit Something " + hitInteract.transform);
-                Debug.DrawRay(transform.position, (transform.forward * 1 * hitInteract.distance), Color.yellow, 10f);
-                InteractObject = hitInteract.transform.GetComponent<IInteractable>();
-                if (InteractObject != null)
+                UITextObj.SetActive(true);
+                UIText.text = hitInteract.transform.name;
+                //         Debug.Log("Did Hit the interactable " + hitInteract.transform);
+                if (KeyDownEvent == true)
                 {
-                    Debug.Log("Did Hit the interactable " + hitInteract.transform);
+                    KeyDownEvent = false;
+                    Debug.Log("Pressed the interact Button");
                     InteractObject.Interact(this);
                 }
             }
-            else
-            {
-                //   Debug.DrawRay(transform.position, (transform.forward * 2), Color.green, 10f);
-                Debug.Log("Did not Hit an interactable");
-            }
-
         }
+        else
+        {
+            //   Debug.DrawRay(transform.position, (transform.forward * 2), Color.green, 10f);
+            //     Debug.Log("Did not Hit an interactable");
+            UITextObj.SetActive(false);
+        }
+
     }
 }
