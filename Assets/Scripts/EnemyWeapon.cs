baa4ca2beb7f728a29c26ef2d528bdde9ecd182c
Nabski89 <Nabski@Hotmail.com>
2023-03-28 21:35:26 -0400

    Door Mapping, Hit Sound

--- Assets/Scripts/EnemyWeapon.cs
+++ Assets/Scripts/EnemyWeapon.cs
@@ -5,16 +5,7 @@ using UnityEngine;
 public class EnemyWeapon : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
+    public GameObject HitSoundEffect;
 
     private void OnTriggerEnter(Collider other)
     {
@@ -25,6 +16,8 @@ public class EnemyWeapon : MonoBehaviour
             Debug.Log(transform.name + " AttackedPlayer");
             HPCheck.HP -= 10;
             enabled = false;
+            if (HitSoundEffect != null)
+                Instantiate(HitSoundEffect, transform.position, transform.rotation);
         }
     }
 }
